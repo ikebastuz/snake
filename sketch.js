a62c8a77d295696b890a9823cd6ac8138e3ea120
ikebastuz <ikebastuz@gmail.com>
2016-12-12 16:40:26 +0200

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,49 @@
+var s;
+var scl = 20;
+var food;
+
 function setup() {
-  createCanvas(600,600);
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  food = createVector(random(width), random(height));
+
 }
 
 function draw() {
   background(51);
+  s.update();
+  s.show();
+
+  fill(255, 0, 100);
+  rect(food.x, food.y);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  }
+}
+
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 0;
+  this.yspeed = 0;
+
+  this.update = function() {
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  }
+
+  this.show = function() {
+    fill(255);
+    rect(this.x, this.y, 10, 10);
+  }
 }

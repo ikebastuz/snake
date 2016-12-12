d0cd2ca56905e7fc6fd0f84e50c58d40785ac6e3
ikebastuz <ikebastuz@gmail.com>
2016-12-12 18:30:31 +0300

    commit

--- sketch.js
+++ sketch.js
@@ -1,49 +1,57 @@
 var s;
 var scl = 20;
+
 var food;
 
 function setup() {
   createCanvas(600, 600);
   s = new Snake();
-  frameRate(10);
-  food = createVector(random(width), random(height));
+  frameRate(20);
+  pickLocation();
 
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
 }
 
 function draw() {
   background(51);
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death();
   s.update();
   s.show();
 
+
   fill(255, 0, 100);
-  rect(food.x, food.y);
+  rect(food.x, food.y, scl, scl);
+  
+  
+text(s.total+1, 600, 10); // Text wraps within text box
 }
+
+
+
+
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
     s.dir(0, -1);
   } else if (keyCode === DOWN_ARROW) {
     s.dir(0, 1);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
   } else if (keyCode === RIGHT_ARROW) {
     s.dir(1, 0);
-  }
-}
-
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 0;
-  this.yspeed = 0;
-
-  this.update = function() {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
-  }
-
-  this.show = function() {
-    fill(255);
-    rect(this.x, this.y, 10, 10);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
   }
 }
